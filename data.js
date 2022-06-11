e731b01438bc9e338faea5cf8d8d470e0ec3f80e
Joe Rupp <joerpp8@gmail.com>
2022-06-11 11:23:37 -0600

    Add CORS and Babel to allow imports

---
+++ data.js
@@ -0,0 +1,30 @@
+const rigsData = [
+  {
+    id: 1,
+    status: "available",
+    name: "NRS Expedition-136 - fully rigged",
+    brand: "NRS",
+    type: "Multi-day",
+    cost: 195,
+    rentStartDate: "",
+    rentEndDate: "",
+    description:
+      "The 13 ft 6in NRS E-136 plays like a 13' boat and works like a 14' boat. Its aggressive rocker and wide stance make it fun but dependable in the rapids. Great for fishing, overnights and just seeking thrills.",
+    photoId: "exampleImage.png",
+  },
+  {
+    id: 2,
+    status: "available",
+    name: "NRS Expedition-136 - fully rigged",
+    brand: "NRS",
+    type: "Multi-day",
+    cost: 195,
+    rentStartDate: "",
+    rentEndDate: "",
+    description:
+      "The 13 ft 6in NRS E-136 plays like a 13' boat and works like a 14' boat. Its aggressive rocker and wide stance make it fun but dependable in the rapids. Great for fishing, overnights and just seeking thrills.",
+    photoId: "exampleImage.png",
+  },
+];
+
+export default rigsData;
--- server.js
+++ server.js
@@ -1,38 +1,15 @@
+import rigsData from "./data.js";
+
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.set("port", process.env.PORT || 3000);
 app.locals.title = "River Renters API";
-app.locals.rigs = [
-  {
-    id: 1,
-    status: "available",
-    name: "NRS Expedition-136 - fully rigged",
-    brand: "NRS",
-    type: "Multi-day",
-    cost: 195,
-    rentStartDate: "",
-    rentEndDate: "",
-    description:
-      "The 13 ft 6in NRS E-136 plays like a 13' boat and works like a 14' boat. Its aggressive rocker and wide stance make it fun but dependable in the rapids. Great for fishing, overnights and just seeking thrills.",
-    photoId: "exampleImage.png",
-  },
-  {
-    id: 2,
-    status: "available",
-    name: "NRS Expedition-136 - fully rigged",
-    brand: "NRS",
-    type: "Multi-day",
-    cost: 195,
-    rentStartDate: "",
-    rentEndDate: "",
-    description:
-      "The 13 ft 6in NRS E-136 plays like a 13' boat and works like a 14' boat. Its aggressive rocker and wide stance make it fun but dependable in the rapids. Great for fishing, overnights and just seeking thrills.",
-    photoId: "exampleImage.png",
-  },
-];
+app.locals.rigs = rigsData;
 
 app.get("/", (request, response) => {
   response.send("Oh hey river rafter");
